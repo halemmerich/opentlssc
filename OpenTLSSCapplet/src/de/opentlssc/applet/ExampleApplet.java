189d00a728870050c9487d19f336955de1895c91
halemmerich <martin.boonk@gmx.net>
2013-02-02 20:55:01 +0100

    preparations for ant build

---
+++ OpenTLSSCapplet/src/de/opentlssc/applet/ExampleApplet.java
@@ -0,0 +1,14 @@
+package de.opentlssc.applet;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISOException;
+import javacardx.apdu.ExtendedLength;
+
+public class ExampleApplet extends Applet implements ExtendedLength {
+
+	public void process(APDU arg0) throws ISOException {
+		// TODO Auto-generated method stub
+
+	}
+}
